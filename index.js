b24df47f37388551a1b9dd2dccc3056e74f41842
Kayla Reopelle <kreopelle@gmail.com>
2018-01-14 12:11:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTo(holiday, name) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days) {
-  return `It's ${days} until ${holiday}!`
+  return `It's ${days} days until ${holiday}!`
 }
